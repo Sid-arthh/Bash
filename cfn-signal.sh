9c828e027f5af0fef9b68c6f10ffac2443f51a92
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-10-12 14:15:54 +0530

    Update cfn-signal.sh

--- cfn-signal.sh
+++ cfn-signal.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
+# count=0
+
+# while [ $count -lt 5 ]; do
+#     http_status=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:80)
+
+#     if [ "$http_status" -ne 200 ]; then
+#         export pass=0
+#         exit 0 
+#     fi
+
+#     count=$((count + 1))
+
+#     if [ $count -lt 5 ]; then
+#         sleep 60
+#     fi
+# done
+# export pass=1
+
 count=0
 
 while [ $count -lt 5 ]; do
-    http_status=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:80)
+ http_status=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:13280/webapi)
 
-    if [ "$http_status" -ne 200 ]; then
-        export pass=0
-        exit 0 
-    fi
+ if [ "$http_status" -eq 200 ]; then
+ /opt/aws/bin/cfn-signal -e 0 --stack $(</opt/aws/bin/stackname) --resource AutoScalingGroup --region ap-southeast-2
+ fi
 
-    count=$((count + 1))
+ count=$((count + 1))
 
-    if [ $count -lt 5 ]; then
-        sleep 60
-    fi
+ if [ $count -lt 5 ]; then
+ sleep 5
+ fi
 done
-export pass=1
+/opt/aws/bin/cfn-signal -e 1 --stack $(</opt/aws/bin/stackname) --resource AutoScalingGroup --region ap-southeast-2
